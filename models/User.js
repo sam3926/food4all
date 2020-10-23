fdadf03e9c1b1b8efa8be318232e7a4b71733610
Krishnendu Sudheesh <krishnendusudheesh4@gmail.com>
2020-10-23 21:28:21 +0530

    User and Donation Schema added

--- models/User.js
+++ models/User.js
@@ -2,15 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Schema
-const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  contact: {
-    type: String,
-    required: true
-  },
+const userSchema = new Schema({
   email: {
     type: String,
     required: true
@@ -19,10 +11,77 @@ const UserSchema = new Schema({
     type: String,
     required: true
   },
-  date: {
+  userType: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  contact: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: { type: String },
+    coordinates: [Number]
+  },
+  donations: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Donation'
+  }],
+
+  followers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  following: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  noFed: {
+    type: Number
+  },
+  noPeople: {
+    type: Number
+  },
+  joinedAt: {
     type: Date,
     default: Date.now
-  }
+  },
+  certificates: [{
+    type: String
+  }],
+  profilePic: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  posts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Post'
+  }],
+  donations: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Donation'
+  }],
+  history: [{
+    color: String,
+    icon: String,
+    text: String
+  }]
+
 });
 
-module.exports = User = mongoose.model("users", UserSchema);
+userSchema.index({ location: "2dsphere" });
+
+module.exports = User = mongoose.model("User", userSchema);

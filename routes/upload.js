a551de0fd4953cf7239ba244fb071dc80f5336e0
Krishnendu Sudheesh <krishnendusudheesh4@gmail.com>
2020-10-24 17:51:29 +0530

    upload routes added

--- routes/upload.js
+++ routes/upload.js
@@ -1,23 +1,11 @@
 const express = require('express')
 const multer = require('multer')
 const fs = require('fs')
-const path = require('path')
+const path = require('path');
+const isAuth = require('../middlewares/isAuth');
 const router = express.Router();
 
 //IMAGE UPLOADS
-
-const imageStorage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const reqPath = path.join(__dirname, '..', 'images')
-        if (!fs.existsSync(reqPath)) {
-            fs.mkdirSync(reqPath, { recursive: true })
-        }
-        cb(null, reqPath)
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname)
-    }
-})
 
 const imageFilter = (req, file, cb) => {
     if (file.mimetype === 'image/png' ||
@@ -30,21 +18,137 @@ const imageFilter = (req, file, cb) => {
     }
 }
 
-const uploadImage = multer({
-    storage: imageStorage,
+const certificatesFilter = (req, file, cb) => {
+    if (file.mimetype === 'application/pdf') {
+        cb(null, true);
+    } else {
+        cb(null, false)
+    }
+}
+
+const certificatesStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const reqPath = path.join(__dirname, '..', 'images', 'certificates')
+        if (!fs.existsSync(reqPath)) {
+            fs.mkdirSync(reqPath, { recursive: true })
+        }
+        cb(null, reqPath)
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+
+
+const profileStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const reqPath = path.join(__dirname, '..', 'images', 'profile')
+        if (!fs.existsSync(reqPath)) {
+            fs.mkdirSync(reqPath, { recursive: true })
+        }
+        cb(null, reqPath)
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+
+
+
+const donationsStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const reqPath = path.join(__dirname, '..', 'images', 'donations')
+        if (!fs.existsSync(reqPath)) {
+            fs.mkdirSync(reqPath, { recursive: true })
+        }
+        cb(null, reqPath)
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+
+
+const postsStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const reqPath = path.join(__dirname, '..', 'images', 'posts')
+        if (!fs.existsSync(reqPath)) {
+            fs.mkdirSync(reqPath, { recursive: true })
+        }
+        cb(null, reqPath)
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+
+
+const uploadCertificates = multer({
+    storage: certificatesStorage,
+    limits: {
+        fieldSize: 3000000
+    },
+    fileFilter: certificatesFilter
+})
+
+
+const uploadProfile = multer({
+    storage: profileStorage,
     limits: {
         fieldSize: 3000000
     },
     fileFilter: imageFilter
 })
 
-//All Image Uploads. Can put multiple routes if needed
-router.post('/', uploadImage.single('file'),
+const uploadPosts = multer({
+    storage: postsStorage,
+    limits: {
+        fieldSize: 3000000
+    },
+    fileFilter: imageFilter
+})
+
+const uploadDonations = multer({
+    storage: donationsStorage,
+    limits: {
+        fieldSize: 3000000
+    },
+    fileFilter: imageFilter
+})
+
+router.post('/certificates', uploadCertificates.single('file'),
     (req, res) => {
         res.json({
-            "location": `http://localhost:8000/images/${req.file.filename}`, "originalName": req.file.originalname
+            "location": `/images/certificates/${req.file.filename}`, "originalName": req.file.originalname
         })
     }
+)
+
+router.post('/profile-', [isAuth, uploadProfile.single('file')],
+    async (req, res) => {
+        //Update the User Model, create an avatar etc.
+        //Get  userid from isAuth
+        res.json("profile pic updated")
+    }
+
+)
+
+router.post('/donations', uploadDonations.single('file'),
+    (req, res) => {
+        res.json({
+            "location": `/images/donations/${req.file.filename}`, "originalName": req.file.originalname
+        })
+    }
+
+)
+
+router.post('/posts', uploadPosts.single('file'),
+    (req, res) => {
+        res.json({
+            "location": `/images/posts/${req.file.filename}`, "originalName": req.file.originalname
+        })
+    }
+
 )
 
 module.exports = router;

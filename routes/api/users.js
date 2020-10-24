1437fd1ca37a1eb30fc5870170a7675964f580b0
Krishnendu Sudheesh <krishnendusudheesh4@gmail.com>
2020-10-24 15:35:10 +0530

    defined all the routes for the profile page

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,8 @@ const keys = require("../../config/keys");
 
 // Load User model
 const User = require("../../models/User");
+const isAuth = require("../../middlewares/isAuth");
+const isOrg = require("../../middlewares/isOrg");
 
 router.post("/register", async (req, res, next) => {
   const { email, name, password, contact, description, address, userType, location } = req.body;
@@ -86,5 +88,153 @@ router.post("/login", async (req, res, next) => {
   }
 
 })
+
+
+router.get('/profile/:id', isAuth, async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id).select('name description contact address userType followers following noFed noPeople')
+    if (user) {
+      console.log(user)
+      res.status(200).json(user)
+    }
+    else {
+      const err = new Error('Profile does not exist!')
+      err.statusCode = 404;
+      throw err
+    }
+
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500
+    }
+    next(err)
+  }
+})
+
+//TODOS
+
+//FOLLOW A USER
+//Can access requesting user's id throud through req.userId
+router.post('/follow/:id', isAuth, async (req, res, next) => {
+  try {
+
+
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500
+    }
+    next(err)
+  }
+})
+
+//EDIT PROFILE
+//Access id only through req.userId, for protection, so other's can't access
+//pass the fields in req.body, find userby id and update those fields only
+//Editing should have name, contact, description, address ,location only
+router.post('/edit-profile', isAuth, async (req, res, next) => {
+  try {
+
+
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500
+    }
+    next(err)
+  }
+})
+
+//CHANGE PROFILE PIC
+
+router.post('/change-pic', isAuth, async (req, res, next) => {
+  try {
+
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500
+    }
+    next(err)
+  }
+})
+
+
+//FETCH USER's donations
+//User's id in route since public , anyone can see
+router.get('/donations/:id', isAuth, async (req, res, next) => {
+  try {
+
+
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500
+    }
+    next(err)
+  }
+})
+
+//FETCH USER's posts
+router.get('/posts/:id', isAuth, async (req, res, next) => {
+  try {
+
+
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500
+    }
+    next(err)
+  }
+})
+
+//FETCH user's history/timeline
+router.get('/history/:id', isAuth, async (req, res, next) => {
+  try {
+
+
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500
+    }
+    next(err)
+  }
+})
+
+//FETCH Suggested pages to be shown in the profile page
+router.get('/suggested-pages', isAuth, async (req, res, next) => {
+  try {
+
+
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500
+    }
+    next(err)
+  }
+})
+
+//Display all the pending donation
+router.get('/pending-donations', isAuth, async (req, res, next) => {
+  try {
+
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500
+    }
+    next(err)
+  }
+})
+
+//Only Organisations
+//Change the status of a pending donation to either accepted or rejected
+//Also take the feedback in the body
+router.post('/pending-donations/:id', isOrg, async (req, res, next) => {
+  try {
+
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500
+    }
+    next(err)
+  }
+})
+
 
 module.exports = router;

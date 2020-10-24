1249b61eacd7bc36f31f2fbd0e6841fe7c814369
Krishnendu Sudheesh <krishnendusudheesh4@gmail.com>
2020-10-24 15:26:33 +0530

    isOrg middleware added

---
+++ middlewares/isOrg.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys');
+
+module.exports = (req, res, next) => {
+    const token = req.get('Authorization');
+    if (!token) {
+        const error = new Error('Not authenticated.');
+        error.statusCode = 401;
+        throw error;
+    }
+
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(token, keys.secretOrKey);
+        console.log("decodedToken", decodedToken)
+    } catch (err) {
+        err.statusCode = 500;
+        throw err;
+    }
+    if (!decodedToken) {
+        const error = new Error('Not authenticated.');
+        error.statusCode = 401;
+        throw error;
+    }
+    if (decodedToken.userType != "organisation") {
+        const error = new Error("Only Organisations allowed");
+        error.statusCode = 401;
+        throw error;
+    }
+    req.userId = decodedToken.userId;
+    req.userType = decodedToken.userType;
+    next();
+};
